d2181cb836f47993d4ec44fb93d544930d3c13e0
Jonathan Lehr <jonathan@aboutobjects.com>
2024-04-09 12:10:22 -0400

    Initial Commit

---
+++ VisionProExamples/VisionProExamplesApp.swift
@@ -0,0 +1,21 @@
+//
+//  VisionProExamplesApp.swift
+//  VisionProExamples
+//
+//  Created by Jonathan Lehr on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
