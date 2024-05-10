419e236159a78dd7a6774b882dfc690bfdbe1142
Jonathan Lehr <jonathan@aboutobjects.com>
2024-05-10 16:43:49 -0400

    Add pitch/yaw modifier and toy rocket entity

--- Transforms/ImmersiveView.swift
+++ Transforms/ImmersiveView.swift
@@ -12,18 +12,45 @@ import RealityKitContent
 struct ImmersiveView: View {
     
     @State private var rootEntity = Entity()
+    @State private var toyPlaneEntity = Entity()
+    @State private var toyRocketEntity = Entity()
+    @State private var shouldPlayAudio = false
     
     var body: some View {
         RealityView { content in
             if let scene = try? await Entity(named: "Toys", in: realityKitContentBundle) {
                 rootEntity = scene
                 content.add(scene)
+                
+                if let entity = rootEntity.findEntity(named: "ToyBiplane") {
+                    print(entity)
+                    toyPlaneEntity = entity
+                    toyPlaneEntity.generateCollisionShapes(recursive: true)
+                    
+                    let parentEntity = Entity()
+                    content.add(parentEntity)
+                    toyPlaneEntity.setParent(parentEntity)
+                }
+                
+                if let entity = rootEntity.findEntity(named: "ToyRocket3") {
+                    print(entity)
+                    toyRocketEntity = entity
+                    toyRocketEntity.generateCollisionShapes(recursive: true)
+                    
+                    let parentEntity = Entity()
+                    content.add(parentEntity)
+                    toyRocketEntity.setParent(parentEntity)
+                }
             }
         } update: { _ in
             Task {
-                await playAirplaneEngineAudio()
+                if shouldPlayAudio {
+                    await playAirplaneEngineAudio()
+                }
             }
         }
+        .pitchAndYaw(targetEntity: toyPlaneEntity, sensitivity: 4)
+        .pitchAndYaw(targetEntity: toyRocketEntity, sensitivity: 4)
     }
     
     @MainActor private func playAirplaneEngineAudio() async {
@@ -38,6 +65,45 @@ struct ImmersiveView: View {
         playbackController.play()
     }
 }
+
+extension View {
+    func pitchAndYaw(targetEntity: Entity, sensitivity: Double = 0.0) -> some View {
+        modifier(PitchAndYawModifier(entity: targetEntity, sensitivity: sensitivity))
+    }
+}
+
+private struct PitchAndYawModifier: ViewModifier {
+    let entity: Entity
+    let sensitivity: Double
+    @State private var yaw = 0.0
+    @State private var baseYaw = 0.0
+    @State private var pitch = 0.0
+    @State private var basePitch = 0.0
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(DragGesture()
+                .targetedToEntity(entity)
+                .onChanged { value in
+                    let location3D = value.convert(value.location3D, from: .local, to: .scene)
+                    let startLocation3D = value.convert(value.startLocation3D, from: .local, to: .scene)
+                    let delta = location3D - startLocation3D
+                    yaw = Double(delta.x) * sensitivity + baseYaw
+                    pitch = Double(delta.y) * sensitivity + basePitch
+                    
+                    let deltaX = simd_quatf(angle: Float(yaw), axis: [0, 1, 0])
+                    let deltaY = simd_quatf(angle: Float(-pitch), axis: [1, 0, 0])
+                    
+                    entity.orientation = deltaY * deltaX
+                }
+                .onEnded { value in
+                    baseYaw = yaw
+                    basePitch = pitch
+                }
+            )
+    }
+}
+
 
 #Preview(immersionStyle: .mixed) {
     ImmersiveView()
